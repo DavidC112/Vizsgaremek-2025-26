a11157f5e165811dd005bdc9fea9206511d8f796
DavidC112 <cornutadavid0@gmail.com>
2026-01-02 14:36:06 +0100

    Planning
    - Created and ER diagrams

--- Vizsgaremek/obj/Debug/net10.0/Vizsgaremek.AssemblyInfo.cs
+++ Vizsgaremek/obj/Debug/net10.0/Vizsgaremek.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vizsgaremek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6f254d724522d8cd4607daf39bbd56f052915d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vizsgaremek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vizsgaremek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
