7b28b2b3879607c773ba3147b05eb6f00195bcb8
Sinkó Dávid <sddavid118@gmail.com>
2026-01-29 09:47:57 +0100

    Added animation

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -2,6 +2,39 @@ import { ArrowLeft } from "lucide-react";
 import Silk from "../components/Silk";
 import Stepper, { Step } from "../components/Stepper";
 import { Link } from "react-router-dom";
+import { motion, type Variants } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
+
+const AnimatedDiv = ({ children }: { children: React.ReactNode }) => {
+  const isDesktop = useMediaQuery({ query: "(min-width: 1280px)" });
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      variants={isDesktop ? desktopVariant : mobileVariant}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+const mobileVariant: Variants = {
+  hidden: { opacity: 0, y: 40 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.4 },
+  },
+};
+
+const desktopVariant: Variants = {
+  hidden: { opacity: 0.9, x: 100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.8, ease: "easeOut" },
+  },
+};
 
 const SignUpPage = () => {
   return (
@@ -26,43 +59,45 @@ const SignUpPage = () => {
         NutriLife
       </section>
 
-      <section className="relative ml-auto min-h-screen w-full space-y-10 rounded-none bg-transparent px-6 shadow-2xl md:w-[50%] md:rounded-2xl md:max-xl:mx-auto md:max-xl:mt-60 md:max-xl:scale-140 md:max-xl:shadow-none xl:bg-white">
-        <h1 className="pt-20 text-center text-3xl font-bold text-white xl:text-black">
-          Sign Up
-        </h1>
+      <AnimatedDiv>
+        <section className="relative ml-auto min-h-screen w-full space-y-10 rounded-none bg-transparent px-6 shadow-2xl md:w-[50%] md:max-xl:mx-auto md:max-xl:mt-60 md:max-xl:scale-140 md:max-xl:rounded-2xl md:max-xl:shadow-none xl:rounded-l-2xl xl:bg-white">
+          <h1 className="pt-20 text-center text-3xl font-bold text-white xl:text-black">
+            Sign Up
+          </h1>
 
-        <Stepper
-          initialStep={1}
-          backButtonText="Previous"
-          nextButtonText="Next"
-        >
-          <Step>
-            <h2>Step 1</h2>
-          </Step>
-
-          <Step>
-            <h2>Step 2</h2>
-          </Step>
-
-          <Step>
-            <h2>Step 3</h2>
-          </Step>
-
-          <Step>
-            <h2>Final Step</h2>
-            <p>You made it!</p>
-          </Step>
-        </Stepper>
-        <h2 className="text-center font-extralight italic max-xl:text-white">
-          Already have an account?{" "}
-          <Link
-            to="/login"
-            className="text-primary-green-50 xl:text-primary-green-500 underline transition-all hover:font-normal"
+          <Stepper
+            initialStep={1}
+            backButtonText="Previous"
+            nextButtonText="Next"
           >
-            Log in
-          </Link>
-        </h2>
-      </section>
+            <Step>
+              <h2>Step 1</h2>
+            </Step>
+
+            <Step>
+              <h2>Step 2</h2>
+            </Step>
+
+            <Step>
+              <h2>Step 3</h2>
+            </Step>
+
+            <Step>
+              <h2>Final Step</h2>
+              <p>You made it!</p>
+            </Step>
+          </Stepper>
+          <h2 className="text-center font-extralight italic max-xl:text-white">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="text-primary-green-50 xl:text-primary-green-500 underline transition-all hover:font-normal"
+            >
+              Log in
+            </Link>
+          </h2>
+        </section>
+      </AnimatedDiv>
     </main>
   );
 };
